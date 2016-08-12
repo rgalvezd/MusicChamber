b14c32aec9b2ae5d48c23d0ab07cc75114198734
Rodrigo Galvez <rodrigogalvezdiaz@outlook.com>
2016-08-12 18:27:18 +0200

    Add mongo connection and index logic

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@
 let express = require("express");
 let app = express();
 
+let mongoUtil = require("./mongoUtil");
+mongoUtil.connect();
+
 app.use( express.static(__dirname + "/../client") );
 
 app.get("/sheets", (request, response) => {
-  response.json( ["Sheet one", "Sheet two", "Sheet three"] );
+  let sheets = mongoUtil.sheets();
+  sheets.find().toArray((err, docs) => {
+    console.log(JSON.stringify(docs));
+    let sheetNames = docs.map((sheet) => sheet.name);
+    response.json( sheetNames );
+  });
 });
 
 app.listen(8181, () => console.log("Listening on port 8181"));
